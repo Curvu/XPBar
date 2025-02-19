41adfbc31f6e38d0c20f640a5d24ea8f7233925b
Curvu <curvu6@gmail.com>
2025-02-19 10:21:58 +0000

    first commit

---
+++ Settings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Diagnostics;
+using System.Drawing;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+using ExileCore2.Shared.Interfaces;
+using ExileCore2.Shared.Nodes;
+
+namespace XPBar
+{
+    public class Settings : ISettings
+    {
+        public Settings()
+        {
+        }
+
+        public ToggleNode Enable { get; set; } = new ToggleNode(true);
+
+        public ToggleNode Debug { get; set; } = new ToggleNode(false);
+    }
+}
