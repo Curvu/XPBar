65082c3935f76c75ba84eb79c71c661f25bf398d
Curvu <curvu6@gmail.com>
2025-02-23 17:51:16 +0000

    no settings needed

--- Settings.cs
+++ Settings.cs
@@ -1,15 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Diagnostics;
-using System.Drawing;
-using System.Numerics;
-using System.Runtime.CompilerServices;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using System.Collections.Generic;
-
-using ExileCore2.Shared.Interfaces;
+﻿using ExileCore2.Shared.Interfaces;
 using ExileCore2.Shared.Nodes;
 
 namespace XPBar
@@ -21,7 +10,5 @@ namespace XPBar
         }
 
         public ToggleNode Enable { get; set; } = new ToggleNode(true);
-
-        public ToggleNode Debug { get; set; } = new ToggleNode(false);
     }
 }
